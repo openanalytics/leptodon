0f8945e77d3efbd06da12a1bf0bb9ce4f45eb639
mverstraete <mverstraete@openanalytics.eu>
2026-03-27 14:51:54 +0100

    fix: wasm32 toolchain

--- nix/flake.nix
+++ nix/flake.nix
@@ -38,6 +38,7 @@
           shellHook = ''
             export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
             export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+            rustup target add wasm32-unknown-unknown
           '';
 
           # Add precompiled library to rustc search path
