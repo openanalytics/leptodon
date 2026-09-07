c06c003a2e5e52ab2ca637db06a771ef6f389ff7
Merlijn Verstraete <merlijn.verstraete@openanalytics.eu>
2026-09-07 07:39:01 +0200

    chore: remove chatty debug messages

--- leptodon/src/popover/mod.rs
+++ leptodon/src/popover/mod.rs
@@ -98,12 +98,9 @@ where
     Effect::watch(
         move || popover_ref.get(),
         move |popover, _, _| {
-            debug_log!("calling move popover handler");
             if let Some(popover) = popover
                 && let Some(body) = use_document().body()
             {
-                debug_log!("adding popover to body");
-
                 body.append_child(popover.deref())
                     .expect("failed to append child to body");
             }
